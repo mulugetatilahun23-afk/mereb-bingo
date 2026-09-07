493196ec072add46d7813a0ea302317e42a130d7
mulugetatilahun23-afk <mulugetatilahun23@gmail.com>
2026-09-07 15:47:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,9 @@ import json
 import re
 import urllib.parse
 from datetime import datetime
-
 app = Flask(name)
-
 Environment Variables
-BOT_TOKEN = os.environ.get("8967099088:AAEpqyu1ZMb8THzF40ZSwFUZxq43dH-oPYA", "")
+BOT_TOKEN = os.environ.get("8967099088:AAEpqyu1ZMb8THzF40ZSwFUZxq43dH-oPYA","")
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "MerebBingoBot")
 DB_NAME = 'mereb_bingo.db'
 
