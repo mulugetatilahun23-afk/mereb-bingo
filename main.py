e02c048ce0ac5b3cf1ecbe526864ffa6ae62f86b
mulugetatilahun23-afk <mulugetatilahun23@gmail.com>
2026-09-08 14:16:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import requests
 
 app = Flask(__name__)
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8967099088:AAEpqyu1ZMb8THzF40ZSWfUZxq43dH-oPYA")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8967099088:AAGREQHetGwLcn5Ie0lHLTg4QbKIWuax5I4")
 
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "MerebBingoBot")
 DB_NAME = 'mereb_bingo.db'
